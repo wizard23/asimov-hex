403789a615deb906dba2bc824ea770ebb2441465
Philipp Tiefenbacher <wizards23+github@gmail.com>
2025-12-22 22:23:10 +0100

    vite config to also generate statistics.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        statistics: resolve(__dirname, 'statistics.html'),
+      },
+    },
+  },
   server: {
     port: 3000,
     open: true
