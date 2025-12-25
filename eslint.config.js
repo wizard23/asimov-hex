3f4fc1c6bcbacabd294b734bec9419f2efcfebf9
Philipp Tiefenbacher <wizards23+github@gmail.com>
2025-12-25 12:02:53 +0100

    add basic linting

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "cache/**",
+      "public/**",
+      "docs/**",
+      "scripts/**",
+      "shell/**"
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+  }
+);
